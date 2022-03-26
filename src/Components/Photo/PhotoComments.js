45bb368682f00500d4e45e28e0830ebf8c43d884
Guilherme <guilherme1067@hotmail.com>
2022-03-26 18:10:56 -0300

    completed the delete button

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import {UserContext} from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = ({id, comments}) => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentSection = useRef(null);
+  const {login} = useContext(UserContext);
+
+  useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+    console.log(commentSection.current)
+  },[comments])
+
   return (
-    <div>PhotoComments</div>
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map(comment => <li key={comment.comment_ID}>
+          <b>{comment.comment_author}: </b>
+          <span>{comment.comment_content}</span>
+        </li>)}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
   )
 }
 
